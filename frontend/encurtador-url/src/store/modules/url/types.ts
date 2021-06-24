08b51e7796e1d75fd23bd0032809d97e98223f11
Lacenlot <gabrieloliveira.17@outlook.com>
2021-06-24 11:40:11 -0300

    removendoFrontEnd

--- frontend/encurtador-url/src/store/modules/url/types.ts
+++
@@ -1,9 +0,0 @@
-export enum UrlTypes {
-    LOAD_URL = 'URL/LOAD_URL',
-}
-
-export interface Url{
-    urlOriginal: string;
-    urlEncurtada: string;
-    dataOperacao: string;
-}
--- frontend/encurtador-url/src/util/notifications/index.ts
+++
@@ -1,8 +0,0 @@
-import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify';
-
-export const errorLogin = () => toast.error('Falha na autenticação, verifique seus dados');
-export const successRegister = () => toast.success('Cadastro efetuado com sucesso!');
-export const errorRegister = () => toast.error('Falha no cadastro, verifique os campos');
-export const successUpdate = () => toast.success('Cadastro atualizado com sucesso!');
-
