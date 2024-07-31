708d36b5ce9e478facbb28ebfce35f8022ebd09d
Gabriel Moreira <gabriel.oliveira@instivo.com.br>
2024-07-31 13:25:03 -0300

    refactor: Removed old source code from application

--- encurtador-url/src/main/java/br/com/lancelot/encurtadorurl/urls/service/interfaces/VerifyUrlService.java
+++
@@ -1,9 +0,0 @@
-package br.com.lancelot.encurtadorurl.urls.service.interfaces;
-
-public interface VerifyUrlService {
-
-    String verifyIfUrlIsValid(String urlToValidate);
-
-    String verifyIfUrlStartsWithHttpOrHttps(String urlToValidate);
-
-}
